1cb0c42d6c2d6a7278625395fff519054ebba5ba
Josexo <jv8784815@gmail.com>
2026-04-15 22:27:37 -0500

    Add download API handler for POST requests

---
+++ api/download.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
+    
+    const API_BASE = 'http://64.20.54.50:30325';
+    
+    try {
+        const response = await fetch(`${API_BASE}/api/download`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(req.body)
+        });
+        
+        const data = await response.json();
+        res.status(response.status).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
