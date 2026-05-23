2a50f487fbc6d0da3d58beb7416570bb4e069b2f
Josexo <jv8784815@gmail.com>
2026-05-23 12:25:08 -0500

    Update info.js

--- api/info.js
+++ api/info.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const { url } = req.query;
-    const API_BASE = 'https://mazier-bounteously-justice.ngrok-free.dev';';
+    const API_BASE = 'https://mazier-bounteously-justice.ngrok-free.dev';
     
     try {
         const response = await fetch(`${API_BASE}/api/info?url=${encodeURIComponent(url)}`);
